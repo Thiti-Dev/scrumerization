4a6893bd6ff8d9b5d5248d63b967178bfc1c796b
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-03-18 14:04:17 +0700

    migration: changes (password) from varchar to text

--- services/core/internal/infrastructure/db/postgres/migration/000002_create_users.up.sql
+++ services/core/internal/infrastructure/db/postgres/migration/000002_create_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     username TEXT UNIQUE CHECK (LENGTH(username) <= 32), -- Nullable
-    password VARCHAR(64) NOT NULL,
+    password TEXT NOT NULL,
     name TEXT,
     is_active BOOLEAN DEFAULT TRUE NOT NULL,
     created_at timestamptz NOT NULL DEFAULT (now()),
