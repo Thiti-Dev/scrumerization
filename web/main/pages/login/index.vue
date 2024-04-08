df08513c5cd22de20c80bd9cc8e0360787c2e301
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-04-08 09:25:06 +0700

    proper font assigns to login,register page

--- web/main/pages/login/index.vue
+++ web/main/pages/login/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <section class="bg-gray-50 dark:bg-gray-900">
+        <section class="bg-gray-50 dark:bg-gray-900 font-workbench">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                     <img class="w-8 h-8 mr-2" src="https://www.svgrepo.com/show/530552/mouthwash-cup.svg" alt="logo">
