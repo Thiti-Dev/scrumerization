61a45a69105342e1ca5ff416f8770833cd22e24b
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-04-08 14:42:56 +0700

    :zap:

--- web/main/codegen.ts
+++ web/main/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:8080/query",
+  schema: process.env.NUXT_PUBLIC_GRAPHQL_ENDPOINT || "http://localhost:8080/query",
   documents: "**/*.vue",
   generates: {
     ".gen/gql/": {
