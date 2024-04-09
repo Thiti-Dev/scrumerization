ecd69f0ca747a3fdadf4b5a4b97b1f67fb8a815a
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-04-09 17:18:17 +0700

    :fire:

--- web/main/pages/registration/index.vue
+++ web/main/pages/registration/index.vue
@@ -11,7 +11,7 @@
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Create and account
                         </h1>
-                    <form @submit.prevent="submitForm" class="space-y-4 md:space-y-6" action="#">
+                    <form @submit.prevent="submitForm" class="space-y-4 md:space-y-6">
                         <div>
                             <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
                             <input v-model="formData.username" :class="{'dark:border-rose-600 border-rose-600': false}" type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe" required>
